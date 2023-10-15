9ee83a698ce938e00e30364a344a6c86c9f4658d
lilkost <111429255+lilkost@users.noreply.github.com>
2023-10-15 22:41:24 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+new Swiper('.slider', {
+    direction: 'vertical',
+    speed: 2400,
+    // mousewheel: {
+    //     enabled: true,
+    //     sensitivity: 3.4,
+    // },
+    mousewheel: true,
+    spaceBetween: 25,
+    parallax: true,
+    // freeMode: true
+});
